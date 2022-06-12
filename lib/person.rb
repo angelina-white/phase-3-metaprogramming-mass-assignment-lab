3cdb7636b91e0a2569823fd044e608665007c775
Angelina <angelinarwhite@gmail.com>
2022-06-11 20:28:43 -0400

    commit tree 00c7ce16236ac7dc10ae36618396632b3e7c0ff7

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Person
   # your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  @@all = []
+
+  def initialize(args)
+    args.each_key do |key, value|
+      self.send("#{key}=", args[key])
+    end
+    @@all << self
+  end
+
+  # pers = {
+  #   name: "Bob",
+  #   hair_color: "brown",
+  #   eye_color: "brown"
+  # }
+
+  # person1 = Person.new(pers)
+
+  # binding.pry
 end
